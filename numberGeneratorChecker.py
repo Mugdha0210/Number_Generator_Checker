cefde89e9569eb772e29a2da336a9a6e28021250
SoumitraTonapi <64733475+SoumitraTonapi@users.noreply.github.com>
2020-05-08 15:52:51 +0530

    Update numberGeneratorChecker.py

--- numberGeneratorChecker.py
+++ numberGeneratorChecker.py
@@ -12,34 +12,34 @@ def obtainNumber():
     engine.setProperty('rate',newVoiceRate)
 
     if numberToWrite == 0:
-        engine.say("Draw Zero")
+        engine.say("Zero")
         engine.runAndWait()
     elif numberToWrite == 1:
-        engine.say("Draw One")
+        engine.say("One")
         engine.runAndWait()
     elif numberToWrite == 2:
-        engine.say("Draw Two")
+        engine.say("Two")
         engine.runAndWait()
     elif numberToWrite == 3:
-        engine.say("Draw Three")
+        engine.say("Three")
         engine.runAndWait()
     elif numberToWrite == 4:
-        engine.say("Draw Four")
+        engine.say("Four")
         engine.runAndWait()
     elif numberToWrite == 5:
-        engine.say("Draw Five")
+        engine.say("Five")
         engine.runAndWait()
     elif numberToWrite == 6:
-        engine.say("Draw Six")
+        engine.say("Six")
         engine.runAndWait()
     elif numberToWrite == 7:
-        engine.say("Draw Seven")
+        engine.say("Seven")
         engine.runAndWait()
     elif numberToWrite == 8:
-        engine.say("Draw Eight")
+        engine.say("Eight")
         engine.runAndWait()
     elif numberToWrite == 9:
-        engine.say("Draw Nine")
+        engine.say("Nine")
         engine.runAndWait()
 
     return numberToWrite
